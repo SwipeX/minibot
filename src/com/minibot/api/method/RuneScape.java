d3994035e9be060da1590be9fafd580108997894
Dogerina <parny_fan2003@doglover.com>
2015-06-29 03:28:58 +0100

    Disable widget rendering!!!1one!!!!eleven!!111

--- src/com/minibot/api/method/RuneScape.java
+++ src/com/minibot/api/method/RuneScape.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class RuneScape {
 
     public static boolean MODEL_RENDERING_ENABLED = true;
+    public static boolean WIDGET_RENDERING_ENABLED = true;
     public static boolean LANDSCAPE_RENDERING_ENABLED = true;
     private static final Queue<Node> queue = new ArrayBlockingQueue<>(2000);
     private static Node last;
