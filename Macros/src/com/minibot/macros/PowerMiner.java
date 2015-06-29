43ac858ec64faa5c1c63ea665d71c1fb42ddafad
TSedlar <sedlarizona@gmail.com>
2015-06-28 23:24:48 -0700

    PowerMiner completion

--- Macros/src/com/minibot/macros/PowerMiner.java
+++ Macros/src/com/minibot/macros/PowerMiner.java
@@ -1,18 +1,19 @@
 package com.minibot.macros;
 
+import com.minibot.api.action.ActionOpcodes;
+import com.minibot.api.action.tree.ExamineEntityAction;
 import com.minibot.api.method.*;
+import com.minibot.api.method.Objects;
 import com.minibot.api.util.Renderable;
 import com.minibot.api.util.Time;
+import com.minibot.api.util.ValueFormat;
 import com.minibot.api.wrapper.locatable.GameObject;
 import com.minibot.api.wrapper.locatable.Tile;
 import com.minibot.bot.macro.Macro;
 import com.minibot.bot.macro.Manifest;
 
 import java.awt.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Tyler Sedlar
@@ -21,39 +22,56 @@ import java.util.Set;
 @Manifest(name = "PowerMiner", author = "Tyler", version = "1.0.0", description = "Powermines surrounding rocks")
 public class PowerMiner extends Macro implements Renderable, ChatboxListener {
 
+    private static final int COMMA_FORMAT = ValueFormat.COMMAS;
+    private static final int THOUSAND_FORMAT = ValueFormat.THOUSANDS | ValueFormat.PRECISION(2);
+
     private final Set<Integer> identifiedRockIds = new HashSet<>();
     private final Set<Integer> validRockIds = new HashSet<>();
 
     private int messageCount = 0;
     private boolean valid = false;
+    private int startExp;
+    private int mined = 0;
+
+    @Override
+    public void atStart() {
+        startExp = Game.experiences()[Skills.MINING];
+    }
 
     @Override
     public void run() {
         Deque<GameObject> objects = Objects.loaded(2);
         for (GameObject object : objects) {
-            int id = object.id();
-            if (!identifiedRockIds.contains(id)) {
-                int cachedMessageCount = messageCount;
-                object.processAction("Examine");
-                while (messageCount == cachedMessageCount)
-                    Time.sleep(50, 100);
-                if (valid)
-                    validRockIds.add(id);
-                identifiedRockIds.add(id);
+            String name = object.name();
+            if (name != null && name.equals("Rocks")) {
+                int id = object.id();
+                if (!identifiedRockIds.contains(id)) {
+                    int cachedMessageCount = messageCount;
+                    RuneScape.processAction(new ExamineEntityAction(ActionOpcodes.EXAMINE_OBJECT, object.uid(),
+                            object.localX(), object.localY()));
+                    while (messageCount == cachedMessageCount)
+                        Time.sleep(50, 100);
+                    System.out.println(id + " " + (valid ? "valid" : "invalid"));
+                    if (valid)
+                        validRockIds.add(id);
+                    identifiedRockIds.add(id);
+                }
             }
         }
-        GameObject rock = Objects.nearestByFilter(o -> validRockIds.contains(o.id()));
+        GameObject rock = Objects.nearestByFilter(o -> validRockIds.contains(o.id()), 2);
         if (rock != null) {
             Inventory.dropAll(i -> {
                 String name = i.name();
                 return name != null && name.toLowerCase().contains("ore");
             });
             rock.processAction("Mine");
+            Time.sleep(() -> Players.local().animation() != -1, 5000);
+            Time.sleep(300, 450);
             Time.sleep(() -> {
                 if (Players.local().animation() == -1)
                     return true;
                 GameObject current = Objects.topAt(rock.location());
-                return current != rock;
+                return current.id() != rock.id();
             }, 5000);
         }
     }
@@ -63,11 +81,22 @@ public class PowerMiner extends Macro implements Renderable, ChatboxListener {
         if (message.contains("outcrop") || message.contains("Stoney!")) {
             messageCount++;
             valid = message.contains("outcrop");
+        } else if (message.contains("manage to mine")) {
+            mined++;
         }
     }
 
     @Override
     public void render(Graphics2D g) {
-
+        g.setColor(Color.CYAN);
+        int gained = Game.experiences()[Skills.MINING] - startExp;
+        int yOff = 11;
+        g.drawString("Runtime: " + Time.format(runtime()), 13, yOff += 15);
+        String fChopped = ValueFormat.format(mined, COMMA_FORMAT);
+        String fChoppedHr = ValueFormat.format(hourly(mined), COMMA_FORMAT);
+        g.drawString("Mined: " + fChopped + " (" + fChoppedHr + "/HR)", 13, yOff += 15);
+        String fExp = ValueFormat.format(gained, COMMA_FORMAT);
+        String fExpHr = ValueFormat.format(hourly(gained), THOUSAND_FORMAT);
+        g.drawString("Experience: " + fExp + " (" + fExpHr + "/HR)", 13, yOff + 15);
     }
 }
