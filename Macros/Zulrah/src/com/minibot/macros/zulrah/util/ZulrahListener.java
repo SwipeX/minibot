f104bd9da4dac8a0274ee7fedc7f29e71666b579
TSedlar <sedlarizona@gmail.com>
2015-07-21 15:45:35 -0700

    updated ZulrahListener inside of Zulrah module

--- Macros/Zulrah/src/com/minibot/macros/zulrah/util/ZulrahListener.java
+++ Macros/Zulrah/src/com/minibot/macros/zulrah/util/ZulrahListener.java
@@ -8,40 +8,45 @@ import com.minibot.bot.macro.LoopTask;
 
 public abstract class ZulrahListener extends LoopTask {
 
-        private Npc npc;
-        private Tile previousTile = null;
-        private int previousId = -1;
+    private Npc npc;
+    private Tile previousTile = null;
+    private int previousId = -1;
+    private boolean wait = false;
 
-        private long lastChange = -1;
-
-        public void setNpc(Npc npc) {
-            this.npc = npc;
-        }
-
-        public boolean validate() {
-            return npc != null;
-        }
-
-        @Override
-        public int loop() {
-            if (npc != null) {
-                Tile tile = npc.location();
-                int id = npc.id();
-                if ((previousId != -1 && id != previousId) || (previousTile != null && (tile.x() != previousTile.x() ||
-                        tile.y() != previousTile.y()))) {
-                    if (lastChange == -1 || Time.millis() - lastChange  > 500) {
-                        lastChange = Time.millis();
-                        onChange(new ZulrahEvent(npc, previousTile, tile, previousId, id));
-                    }
-                }
-                previousTile = tile;
-                previousId = id;
-            } else {
-                previousTile = null;
-                previousId = -1;
-            }
-            return Random.nextInt(25, 50);
-        }
-
-        public abstract void onChange(ZulrahEvent event);
+    public void setNpc(Npc npc) {
+        this.npc = npc;
     }
+
+    public boolean validate() {
+        return npc != null;
+    }
+
+    @Override
+    public int loop() {
+        if (npc != null) {
+            Tile tile = npc.location();
+            int id = npc.id();
+            int height = npc.raw().getHeight();
+            if (height == 0) {
+                if (!wait) {
+                    previousTile = tile;
+                    previousId = id;
+                    wait = true;
+                }
+            } else {
+                if (wait && height > 100) {
+                    onChange(new ZulrahEvent(npc, previousTile, tile, previousId, id));
+                    previousTile = tile;
+                    previousId = id;
+                    wait = false;
+                }
+            }
+        } else {
+            previousTile = null;
+            previousId = -1;
+        }
+        return Random.nextInt(25, 50);
+    }
+
+    public abstract void onChange(ZulrahEvent event);
+}
